7f7d9dbbdc995e793927a0face56c9e6bc2889e0
SanaYasfp <sanayasfp@gmail.com>
2022-12-30 16:31:05 +0000

    u

--- src/apis/auth/controller.ts
+++ src/apis/auth/controller.ts
@@ -375,7 +375,7 @@ export const twoFACheck = async (req: Request, res: Response) => {
     let data = req.body;
 
     const result = serializer(data, {
-        code: 'not_null, number',
+        code: 'integer',
         authorization_code: "not_null",
         code_verifier: "not_null",
     });
@@ -600,7 +600,7 @@ export const checkVerification = async (req: Request, res: Response) => {
     let data = req.body;
 
     const result = serializer(data, {
-        code: 'number',
+        code: 'integer',
         authorization_code: "not_null",
         code_verifier: "not_null, optional",
     });
