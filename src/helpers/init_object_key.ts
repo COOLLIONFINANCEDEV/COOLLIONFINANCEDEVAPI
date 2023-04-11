29b71bf6f758369649385decca72ab9b37993c73
SanaYasfp <sanayasfp@gmail.com>
2023-04-11 11:26:16 +0000

    update

--- src/helpers/init_object_key.ts
+++
@@ -1,9 +0,0 @@
-function init_object_key({ obj, key, defaultValue = {} }: { obj: { [x: string]: any; }; key: keyof typeof obj; defaultValue?: any; }) {
-    if (obj[key] == undefined)
-        obj[key] = defaultValue;
-
-    return obj;
-}
-
-export default init_object_key;
-
