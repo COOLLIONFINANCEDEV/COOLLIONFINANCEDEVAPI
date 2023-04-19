c0c86f1cd894d32fce807ea1dd84b25452a72a92
SanaYasfp <yasfp.pro@gmail.com>
2023-04-19 14:11:28 +0000

    refactor: remove authorization on project list route

--- src/routes/project.route.ts
+++ src/routes/project.route.ts
@@ -7,7 +7,7 @@ import * as controller from '../controllers/project.controller';
 const router = express.Router();
 const { register, update, remove, retrive, list } = endpoints;
 
-router[list.method](list.path, authenticate, authorize(list.authorizationRules), controller.list);
+router[list.method](list.path, /* authenticate, authorize(list.authorizationRules), */ controller.list);
 router[retrive.method](retrive.path, authenticate, authorize(retrive.authorizationRules), controller.retrive);
 router[remove.method](remove.path, authenticate, authorize(remove.authorizationRules), controller.remove);
 router[update.method](update.path, authenticate, authorize(update.authorizationRules), validator(update.schema), controller.update);
