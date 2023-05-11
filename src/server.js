e32b5f4b909b21f48e451ce059e89bd988877662
coollionfinance1 <115308535+coollionfinance1@users.noreply.github.com>
2023-05-11 09:46:55 +0000

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import http from 'http';
+import normalizePort from 'normalize-port-2';
+
+import app from 'src/app';
+import { server as serverConfig } from 'config/index';
+
+// Obtenir le port de l'environnement et le stockez dans Express.
+const port = normalizePort(serverConfig.port || '3000');
+// app.set('port', port);
+
+// Créer un serveur HTTP.
+// const server = http.createServer(app)
+//     .listen(port, () => {
+//         console.log(`Server running: http://${serverConfig.host}:${port}.`);
+//     });
+
+// app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+const server = app.listen(port, () => {
+    // @ts-ignore
+    console.log(`App listenning at ${server.address().address}:${port}`);
+});
